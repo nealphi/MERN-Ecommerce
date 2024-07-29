436a0f3d272fe0dfdf642be02b91f1c3c975611b
Negin Alphi <negin.alipanahi@gmail.com>
2024-07-29 17:12:21 -0400

    api / added

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,16 +6,19 @@ import { productRouter } from "./routes/product";
 
 const app = express();
 
-app.use(cors({
-  origin: ["https://mern-ecommerce-server-flax.vercel.app"],
-  methods: ["POST","GET"],
-  credentials: true
-}))
+// app.use(cors({
+//   origin: ["https://mern-ecommerce-server-qt50yunnr-nealphis-projects.vercel.app"],
+//   methods: ["POST","GET"],
+//   credentials: true
+// }))
 
 app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
+app.get("/", (req, res) => {
+  res.json("hello");
+});
 
 mongoose.connect(
   "mongodb+srv://neginalipanahi:N1e-g2i%2Fn1368@ecommerce.v1fle7p.mongodb.net/ecommerce"
