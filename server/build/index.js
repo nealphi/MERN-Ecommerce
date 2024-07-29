23e5081ff06292520922f3b55e2dc80c1b0f8675
Negin Alphi <negin.alipanahi@gmail.com>
2024-07-29 15:16:21 -0400

    versel.json deleted

---
+++ server/build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const user_1 = require("./routes/user");
+const product_1 = require("./routes/product");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use("/user", user_1.userRouter);
+app.use("/product", product_1.productRouter);
+mongoose_1.default.connect("mongodb+srv://neginalipanahi:N1e-g2i%2Fn1368@ecommerce.v1fle7p.mongodb.net/ecommerce");
+app.listen(3001, () => console.log("Server started"));
