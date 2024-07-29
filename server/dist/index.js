e40d59b7059a82f9131454a781a8c52e97dcc8a1
Negin Alphi <negin.alipanahi@gmail.com>
2024-07-29 17:47:06 -0400

    tsc config updated

--- server/dist/index.js
+++ server/dist/index.js
@@ -9,9 +9,16 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const user_1 = require("./routes/user");
 const product_1 = require("./routes/product");
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: ["https://mern-ecommerce-server-flax.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
 app.use("/user", user_1.userRouter);
 app.use("/product", product_1.productRouter);
+app.get("/hello", (req, res) => {
+    res.json("hello");
+});
 mongoose_1.default.connect("mongodb+srv://neginalipanahi:N1e-g2i%2Fn1368@ecommerce.v1fle7p.mongodb.net/ecommerce");
 app.listen(3001, () => console.log("Server started"));
